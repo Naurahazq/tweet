b369eeb1576ceecca4676aa20353b477255a39eb
Naurahazq <naurahazzahra.23@gmail.com>
2020-02-20 21:11:16 +0700

    nau

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,45 +1,35 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-3">
-            <!-- posisi kiri -->
-        </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+            </div>
             <div class="col-md-6">
                 <div class="card border-0">
-                    <div class="d-flex card-header border-0">
-                        <div class="mr-auto">
-                            <h6 class="text-muted">Tweet</h6>
-                        </div>
-                    <div>
-                    <svg fill="none" height="24" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><circle cx="12" cy="12" r="2"/><circle cx="12" cy="4" r="2"/><circle cx="12" cy="20" r="2"/></svg>
-                </div>
-            </div>
-            <div class="card-body">
-                <div>
-                    <div class="d-flex">
+                    <div class="card-body">
+                        <div class="d-flex">
                         <img src="{{asset('images/bird.jpg')}}" width="50" height="50" class="rounded-circle" alt="">
-                            <h6 class="text-dark">
+                                <div class="ml-3">
+                                <h6 class="text-dark">
                                 Naurah
-                            <h6 class="text-muted">@naurah</h6>
-                        </h6>
-                        <p class="text-primary">@nau,
-                            <span class="text-muted">
+                                <small class="text-muted">@naurahazz
+                                </small>
+                            </h6>
+                            <h6 class="text-primary">@nau,
+                                <span class="text-muted">
                                 Lorem ipsum dolor sit amet,
-                                consectetur adipiscing elit,
-                                sed do
-                            </span>
-                        </p>
+                                consectetur adipisching elit,
+                                sad do.
+                                </span>
+                            </h6>
+                        </div>
                     </div>
                 </div>
-            
-           </div> 
-        </div>
-    </div>
-</div>
+            </div>
+        </div>   
             <div class="col-md-3">
-                <!-- posisi kanan -->
+            </div>
         </div>
     </div>
 </div>
